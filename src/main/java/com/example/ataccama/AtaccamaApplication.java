de1804fecd802f5e31c0aca0eb418a3902c90121
vzlamal <zlamavit@gmail.com>
2021-01-09 14:36:34 +0100

    init

---
+++ src/main/java/com/example/ataccama/AtaccamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ataccama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaccamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaccamaApplication.class, args);
+	}
+
+}
